c0647f80aca00000da399173230c0eb47ec2a13b
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2020-05-12 19:59:43 +0200

    Fix rootfs init scripts. v2

--- sd_card/replace_rootfs.sh
+++ sd_card/replace_rootfs.sh
@@ -15,8 +15,9 @@ sudo cp -R ./rootfs_old/lib/modules/* ./rootfs/lib/modules/
 
 echo "Moving SSH config files" # target needs 'haveged' package to create entropy faster, otherwise hangs on sshd startup
 cp ./rootfs_old/etc/ssh/sshd_config ./rootfs/etc/ssh/sshd_config
-echo "Moving Ethernet setup"
+echo "Moving Ethernet and sshd init fixes"
 cp ./rootfs_old/etc/init.d/S99eth0 ./rootfs/etc/init.d/S99eth0
+cp ./rootfs_old/etc/init.d/S49sshd-fix ./rootfs/etc/init.d/S49sshd-fix
 
 echo "Removing old rootfs"
 sudo rm -rf ./rootfs_old
