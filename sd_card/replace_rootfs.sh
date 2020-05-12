c1a1250e80ceeb7a8694080e9f39d419e9f90909
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2020-05-12 17:02:33 +0200

    Add Scripts for easier SSH setup. Add copying from old rootfs to speedup development.

--- sd_card/replace_rootfs.sh
+++ sd_card/replace_rootfs.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/bash
-sudo rm -rf ./rootfs
+echo "Renaming old rootfs"
+mv rootfs rootfs_old
 mkdir rootfs
+
+echo "Unpacking new rootfs"
 tar -xvf ../software/buildroot/output/images/rootfs.tar -C ./rootfs
-sudo chown root:root ./rootfs
+#sudo chown root:root ./rootfs is this step needed?
+# copy the modules
+
+echo "Copying kernel modules"
+mkdir -p ./rootfs/lib/modules
+sudo cp -R ./rootfs_old/lib/modules/* ./rootfs/lib/modules/
+# For now it is empty... sudo cp ./rootfs_old/root/* ./rootfs/root/
+
+echo "Moving SSH config files" # target needs 'haveged' package to create entropy faster, otherwise hangs on sshd startup
+cp ./rootfs_old/etc/ssh/sshd_config ./rootfs/etc/ssh/sshd_config
+echo "Moving Ethernet setup"
+cp ./rootfs_old/etc/init.d/S99eth0 ./rootfs/etc/init.d/S99eth0
+echo "Chown of /dev/zero change for ssh"
+sudo chown root:root ./rootfs/dev/zero
+
+echo "Removing old rootfs"
+sudo rm -rf ./rootfs_old
