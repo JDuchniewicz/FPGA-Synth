86178d1ac24c3b4925410e4146344f9ac8d4be21
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2020-05-13 15:23:59 +0200

    Added writing to FPGA character file.

---
+++ midireader/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_/mnt/games/Projects/De0Nano/atlas_linux_ghrd/software/bachelor_module/midireader/built-in.o :=  rm -f /mnt/games/Projects/De0Nano/atlas_linux_ghrd/software/bachelor_module/midireader/built-in.o; arm-linux-gnueabihf-ar rcSTPD /mnt/games/Projects/De0Nano/atlas_linux_ghrd/software/bachelor_module/midireader/built-in.o
---
+++ midireader/.tmp_versions/midi_reader.mod
@@ -0,0 +1,3 @@
+/mnt/games/Projects/De0Nano/atlas_linux_ghrd/software/bachelor_module/midireader/midi_reader.ko
+/mnt/games/Projects/De0Nano/atlas_linux_ghrd/software/bachelor_module/midireader/midi_reader.o
+
