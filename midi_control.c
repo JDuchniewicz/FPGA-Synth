61406e7f05e53af4b467efbdd3c26cc7934b36e4
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2020-05-14 14:15:02 +0200

    Add writing to device file.

--- midi_control.c
+++ midi_control.c
@@ -7,6 +7,7 @@
 static snd_seq_t* seq;
 static snd_seq_addr_t* port;
 static volatile sig_atomic_t stop = 0;
+static int fpga_fd = 0;
 
 static void fatal(const char* msg, ...)
 {
@@ -66,22 +67,59 @@ static void connect_port(void)
               port->client, port->port, snd_strerror(err));
 }
 
+static unsigned int create_fpga_command(int on, unsigned char note, unsigned char velocity)
+{
+    unsigned int command = 0x0;
+    command |= note;
+    command <<= 8;
+    command |= velocity;
+    command |= (on << 15); // the order is important because of the types
+    //printf("Command is: %x\n", command);
+    return command;
+}
+
+static void write_device(unsigned int* command)
+{
+    printf("Writing a command %x to the /dev/synthesizer\n", *command);
+    if (write(fpga_fd, command, sizeof(unsigned int)) < sizeof(unsigned int))
+        fatal("Could not write command %x to the /dev/synthesizer\n", *command);
+}
+
+// This function has to command the FPGA firstly decoding the note into the most important parts
+// TODO: create driver for FPGA with charfile creation and define memory regions from which we can read processed output
+// Commands should be truncated and concatenated into appropriate messages to be sent to the driver, which will then transfer the commands to the char file
+// moreover interrupt should be signalled to start/stop whole pipeline of substractive synthesis
+// should this userspace application obtain this audio via DMA and output it somewhere or should it be done only in driver - which will then supply it to any application that waits for it?
 static void handle_event(snd_seq_event_t* ev)
 {
+    unsigned int command = 0x0;
     printf("%3d:%-3d ", ev->source.client, ev->source.port);
     switch (ev->type) {
         case SND_SEQ_EVENT_NOTEON:
             if (ev->data.note.velocity)
+            {
                 printf("Note on             %2d, note %d, velocity %d\n",
                        ev->data.note.channel, ev->data.note.note, ev->data.note.velocity);
-            else
+                // write to FPGA
+               command = create_fpga_command(1, ev->data.note.note, ev->data.note.velocity);
+               write_device(&command);
+            }
+            else // never triggers?
+            {
                 printf("Note off             %2d, note %d",
                        ev->data.note.channel, ev->data.note.note);
+               command = create_fpga_command(0, ev->data.note.note, 0x0);
+               write_device(&command);
+            }
             break;
         case SND_SEQ_EVENT_NOTEOFF:
-            printf("Note on             %2d, note %d, velocity %d\n",
+        {
+            printf("Note off %2d, note %d, velocity %d\n",
                     ev->data.note.channel, ev->data.note.note, ev->data.note.velocity);
+            command = create_fpga_command(0, ev->data.note.note, ev->data.note.velocity);
+            write_device(&command);
             break;
+        }
         default:
             printf("Event type: %d\n", ev->type);
     }
@@ -92,6 +130,14 @@ int main(int argc, char* argv[])
     int err;
     struct pollfd* pfds;
     int npfds;
+    if (argc < 2) // argv[1] is the port num to listen on
+        fatal("Please pass a port to listen on!\n");
+
+    // open FPGA device file
+    fpga_fd = open("/dev/synthesizer", O_WRONLY);
+    if (fpga_fd < 0)
+        fatal("Could not open the FPGA char file!\n");
+
     // for now hardcode stuff
     // initialize the sequencer object
     init_seq();
@@ -100,9 +146,9 @@ int main(int argc, char* argv[])
     port = realloc(port, sizeof(snd_seq_addr_t));
     mem_check(port);
 
-    err = snd_seq_parse_address(seq, port, "16");
+    err = snd_seq_parse_address(seq, port, argv[1]);
     if (err < 0)
-        fatal("Invalid port %s - %s", "16", snd_strerror(err));
+        fatal("Invalid port %s - %s", argv[1], snd_strerror(err));
 
     // create the port object
     create_port();
@@ -144,6 +190,7 @@ int main(int argc, char* argv[])
             break;
     }
     
+    close(fpga_fd);
     snd_seq_close(seq);
     return 0;
 }
